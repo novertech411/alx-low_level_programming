ac54d286d3b80e18cb5b4f4eb4fb00799489e2ea
novetech411 <novertech4@gmail>
2022-10-13 21:52:54 -0700

    task 6, 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+gcc -S -masm=intel $CFILE
+
+Task 8
+Vi 101-quote.c
+
+#include <unistd.h>
+/**
+ *main-program entry point.
+ *
+ *Return: 1 on success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
